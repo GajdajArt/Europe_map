acfb0254312142d6aac9ea88f24f7f603471b8cf
GajdajArt <Gajdajart@gmail.com>
2018-06-09 20:33:07 +0300

    new method of creating arrays

--- app/src/main/java/com/labralab/europemap/MainActivity.java
+++ app/src/main/java/com/labralab/europemap/MainActivity.java
@@ -1,10 +1,12 @@
 package com.labralab.europemap;
 
 import android.content.DialogInterface;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
+import android.provider.CalendarContract;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,11 +15,17 @@ import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.labralab.europemap.databinding.ActivityMainBinding;
 
+import java.lang.reflect.Field;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    String[] countryTitles;
+    int[] countryColors;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,82 +39,88 @@ public class MainActivity extends AppCompatActivity {
         //Скрываем статус бар
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
 
-        //Масив названий стран
-        final String[] countryTitles = {getString(R.string.finland),
-                getString(R.string.estonia), getString(R.string.latvia), getString(R.string.lithuania),
-                getString(R.string.belarus), getString(R.string.ukraine), getString(R.string.moldova),
-                getString(R.string.sweden), getString(R.string.poland), getString(R.string.slovakia),
-                getString(R.string.hungary), getString(R.string.romania), getString(R.string.bulgaria),
-                getString(R.string.macedonia), getString(R.string.greece), getString(R.string.norway),
-                getString(R.string.denmark), getString(R.string.germany), getString(R.string.czech_rep),
-                getString(R.string.austria), getString(R.string.slovenia), getString(R.string.croatia),
-                getString(R.string.bos_her), getString(R.string.serbia), getString(R.string.montenegro),
-                getString(R.string.kosovo), getString(R.string.albania), getString(R.string.switzerland),
-                getString(R.string.italy), getString(R.string.uk), getString(R.string.ireland),
-                getString(R.string.netherlands), getString(R.string.belgium), getString(R.string.lux),
-                getString(R.string.france), getString(R.string.spain), getString(R.string.portugal),
-        };
-        //Масив цветов в порядке соответствия названиям стран
-        final int[] countryColors = {getResources().getColor(R.color.finland),
-                getResources().getColor(R.color.estonia),
-                getResources().getColor(R.color.latvia), getResources().getColor(R.color.lithuania),
-                getResources().getColor(R.color.belarus), getResources().getColor(R.color.ukraine),
-                getResources().getColor(R.color.moldova), getResources().getColor(R.color.sweden),
-                getResources().getColor(R.color.poland), getResources().getColor(R.color.slovakia),
-                getResources().getColor(R.color.hungary), getResources().getColor(R.color.romania),
-                getResources().getColor(R.color.bulgaria), getResources().getColor(R.color.macedonia),
-                getResources().getColor(R.color.greece), getResources().getColor(R.color.norway),
-                getResources().getColor(R.color.denmark), getResources().getColor(R.color.germany),
-                getResources().getColor(R.color.czech_rep), getResources().getColor(R.color.austria),
-                getResources().getColor(R.color.slovenia), getResources().getColor(R.color.croatia),
-                getResources().getColor(R.color.bos_her),
-                getResources().getColor(R.color.serbia), getResources().getColor(R.color.montenegro),
-                getResources().getColor(R.color.kosovo), getResources().getColor(R.color.albania),
-                getResources().getColor(R.color.switzerland), getResources().getColor(R.color.italy),
-                getResources().getColor(R.color.uk), getResources().getColor(R.color.ireland),
-                getResources().getColor(R.color.netherlands), getResources().getColor(R.color.belgium),
-                getResources().getColor(R.color.lux), getResources().getColor(R.color.france),
-                getResources().getColor(R.color.spain), getResources().getColor(R.color.portugal),};
+        initArrays();
 
         //Находим mapView и обрабатываем касание
         final ImageView mapView = (ImageView) findViewById(R.id.mapView);
-        mapView.setOnTouchListener(new ImageView.OnTouchListener() {
+        mapView.setOnTouchListener(new ImageView.OnTouchListener()
+
+        {
 
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-
-                //В случае отпускания
-                if (event.getAction() == MotionEvent.ACTION_UP) {
-                    int x = Math.round(event.getX());
-                    int y = Math.round(event.getY());
-
-                    //Получаем Bitmap с учетом того изображения которе вне области видимости
-                    LayoutInflater inflater = (LayoutInflater) MainActivity.this.getSystemService(LAYOUT_INFLATER_SERVICE);
-                    inflater.inflate(R.layout.activity_main, null).setDrawingCacheEnabled(true);
-                    Bitmap bitmap = getBitmapFromView(MainActivity.this.getWindow().findViewById(R.id.mapView));
-                    inflater.inflate(R.layout.activity_main, null).setDrawingCacheEnabled(false);
-
-
-                    //Получаем пиксель подприкосновением
-                    int pixel = bitmap.getPixel(x, y);
-
-                    //Убиваем Bitmap (на всякий случай)
-                    bitmap = null;
-
-                    //позиция цвета и нзвания страны в массивах
-                    int countryID = findCountry(pixel, countryColors);
-
-                    //Если цвет пикселя соответствует какой либо стране
-                    if (countryID > -1) {
-
-                        //Запускаем диалог с названием выбранной страны
-                        dialogCreate(countryTitles[countryID]);
-                    }
-                }
+                checkCountry(event);
                 return true;
             }
         });
     }
+
+    private void checkCountry(MotionEvent event){
+        //В случае отпускания
+        if (event.getAction() == MotionEvent.ACTION_UP) {
+            int x = Math.round(event.getX());
+            int y = Math.round(event.getY());
+
+            //Получаем Bitmap с учетом того изображения которе вне области видимости
+            LayoutInflater inflater = (LayoutInflater) MainActivity.this.getSystemService(LAYOUT_INFLATER_SERVICE);
+            inflater.inflate(R.layout.activity_main, null).setDrawingCacheEnabled(true);
+            Bitmap bitmap = getBitmapFromView(MainActivity.this.getWindow().findViewById(R.id.mapView));
+            inflater.inflate(R.layout.activity_main, null).setDrawingCacheEnabled(false);
+
+
+            //Получаем пиксель подприкосновением
+            int pixel = bitmap.getPixel(x, y);
+
+            //Убиваем Bitmap (на всякий случай)
+            bitmap = null;
+
+            //позиция цвета и нзвания страны в массивах
+            int countryID = findCountry(pixel, countryColors);
+
+            //Если цвет пикселя соответствует какой либо стране
+            if (countryID > -1) {
+
+                //Запускаем диалог с названием выбранной страны
+                dialogCreate(countryTitles[countryID]);
+            }
+        }
+    }
+
+    private void initArrays(){
+
+        int result = 1;
+
+        countryTitles = getResources().getStringArray(R.array.country_title_list);
+        countryColors = getResources().getIntArray(R.array.country_colors_list);
+
+
+//        for (int i = 0; i < countryTitles.length; i++) {
+//
+//            int currentStringId =  getResources().getIdentifier("uk", "strings", this.getPackageName());
+//            String currentIdName = getResources().getResourceName(currentStringId);
+//            int currentColorId = getColorId(currentIdName);
+//            if (currentColorId != -1) {
+//                countryColors[i] = getResources().getColor(currentColorId);
+//            } else {
+//                result = currentColorId;
+//                break;
+//            }
+//        }
+
+    }
+
+//    private int getColorId(String resName) {
+//
+//        int result;
+//        try {
+//            Field idField = R.color.class.getDeclaredField(resName);
+//            result = idField.getInt(idField);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            result = -1;
+//        }
+//        return result;
+//    }
 
     //Метод для получения Bitmap с учетом scrollView
     private Bitmap getBitmapFromView(View view) {
